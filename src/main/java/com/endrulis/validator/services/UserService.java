15e5397a895e8ceca3efad04e10bd6967c4749b7
Endrulis <endrulisr@gmail.com>
2023-03-19 14:50:20 +0200

    Removed unused files

--- src/main/java/com/endrulis/validator/services/UserService.java
+++ src/main/java/com/endrulis/validator/services/UserService.java
@@ -1,10 +1,7 @@
 package com.endrulis.validator.services;
 
 import com.endrulis.validator.dto.UserDTO;
-import com.endrulis.validator.dto.UserRegistrationDTO;
 import com.endrulis.validator.entities.UserEntity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
--- src/main/java/com/endrulis/validator/services/UserServiceImpl.java
+++ src/main/java/com/endrulis/validator/services/UserServiceImpl.java
@@ -1,22 +1,14 @@
 package com.endrulis.validator.services;
 
 import com.endrulis.validator.dto.UserDTO;
-import com.endrulis.validator.dto.UserRegistrationDTO;
 import com.endrulis.validator.entities.RoleEntity;
 import com.endrulis.validator.entities.UserEntity;
 import com.endrulis.validator.repositories.RoleRepository;
 import com.endrulis.validator.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
